db0cecf60ddd5f6be90747cf98ebd7d08f6f0f25
you@example.com <you@example.com>
2025-12-05 08:57:22 +0200

    Made HTML boiler plate, array, and foreach cycle in PHP

--- about.php
+++ about.php
@@ -1,1 +1,19 @@
-<!-- your code goes here -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>About</h1>";
+    $indexedArray = ["Lorem ipsum dolor sit amet.", 
+    "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, iusto."];
+    foreach($indexedArray as $text){
+        echo "<p>$text</p>";
+    };
+    ?>
+</body>
+</html>
